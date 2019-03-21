15c3af030f429daa31b9c8a0719610fe8fbad9e9
Andu Morie <andu.morie@uipath.com>
2019-03-21 11:54:44 +0200

    Created script to auto-download the ML model. Changed the process to a generic message box.

--- Prerequisites/get_python_path.py
+++ Prerequisites/get_python_path.py
@@ -1,3 +1,4 @@
+import requests
 import sys
 
 for path in sys.path:
@@ -6,3 +7,17 @@ for path in sys.path:
             f.write("PythonPath,{}".format(path))
             print("Python Path written to config file.")
             break
+
+
+print("Downloading ML model... Please don't close this window.")
+
+url = "https://github.com/andumorie/uipath-face-recognition-framework/raw/master/FaceRecognition/model/20170511-185253.pb"
+r = requests.get(url)
+
+with open('../FaceRecognition/model/20170511-185253.pb', 'wb') as f:  
+    f.write(r.content)
+
+if r.status_code == 200:
+    print("Download completed successfully!")
+else:
+    print("An error occured while downloading the model. Please check your internet connection and try again.")
